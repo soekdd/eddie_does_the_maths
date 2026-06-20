1108dea5d0bf092577c40c8e9497952e2b11b864
Soeren Kahl <soek@scripsi.de>
2026-06-20 08:48:15 +0200

    image for RC added

--- src/book1/RC/RC.vue
+++ src/book1/RC/RC.vue
@@ -5,6 +5,19 @@
 	:title="t( 'title' )"
 	:vue-date="__VITE_SFC_MTIME_MS__"
 >
+	<template #bookPart>
+		<figure class="exampleFigure">
+			<ImageZoomer is-ai :title="t( 'imageTitle' )">
+				<img :alt="t( 'imageAlt' )" loading="lazy" :src="titleImg" />
+			</ImageZoomer>
+		</figure>
+		<h3 id="einleitung">{{ t( "introDate" ) }}</h3>
+		<div class="eddie">
+			<p v-html="t( 'book.p1' )"/>
+			<p v-html="t( 'book.p2' )"/>
+			<p v-html="t( 'book.p3' )"/>
+		</div>
+	</template>
 	<template #descriptionPart>
 		<RCRubikCube />
 	</template>
@@ -14,6 +27,7 @@
 <script setup>
 import { useI18n } from "@/utils/i18n.mjs";
 import RCRubikCube from "./RC_RubikCube.vue";
+import titleImg from "./RC.webp";
 
 const { t } = useI18n( "book1.RC" );
 </script>
